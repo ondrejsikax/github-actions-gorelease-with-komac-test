49f966de51c1157d6e353c11d4576e9e3af01883
Ondrej Sika <ondrej@ondrejsika.com>
2026-03-11 22:43:01 +0100

    feat: create simple go app (hello world)

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ondrejsikax/github-actions-gorelease-with-komac-test
+
+go 1.25.0
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+var Version = "dev"
+
+func main() {
+	fmt.Println("Hello World!", Version)
+}
